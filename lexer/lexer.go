8d1e927fb45b896fd1641e72ec43954dbe85dbcf
mdm-code <code.madamczyk@gmail.com>
2023-11-08 21:33:42 +0100

    Implement basic lexer functionality

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,44 +1,85 @@
 // Package lexer ...
+//
+// It's difficult but let's decide that all keys should be quoted. This makes
+// allowed toml only-digits keys explicit. Dotted keys in toml will be handled
+// with ["foo"]["bar"] syntax of a query.
 package lexer
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 
 	"github.com/mdm-code/scanner"
 )
 
 const (
-	// IDENT ...
-	IDENT TokenType = iota
+	// String ...
+	String TokenType = iota
+
+	// Integer ...
+	Integer
+
+	// Dot ...
+	Dot
+
+	// Comma ...
+	Comma
+
+	// Colon ...
+	Colon
+
+	// ArrayOpen ...
+	ArrayOpen
+
+	// ArrayClose ...
+	ArrayClose
+
+	// Whitespace ...
+	Whitespace
+
+	// Undef ...
+	Undef
 )
+
+// KeyCharMap ...
+var KeyCharMap = map[rune]TokenType{
+	'.': Dot,
+	',': Comma,
+	':': Colon,
+	'[': ArrayOpen,
+	']': ArrayClose,
+}
 
 // ErrNilScanner ...
 var ErrNilScanner = errors.New("provided Scanner is nil")
 
-// Zero ...
-var Zero = Pos{Tokens: []scanner.Token{}, Start: 0, End: 0}
+// ErrKeyCharUnsupported ...
+var ErrKeyCharUnsupported = errors.New("unsupported key character")
+
+// ErrUnterminatedString ...
+var ErrUnterminatedString = errors.New("unterminated string literal")
+
+// ErrDisallowedChar ...
+var ErrDisallowedChar = errors.New("disallowed character")
 
 // TokenType ...
 type TokenType uint8
 
-// Pos ...
-type Pos struct {
-	Tokens     []scanner.Token
-	Start, End int
-}
-
 // Token ...
 type Token struct {
-	Pos
-	Buffer *[]scanner.Token
-	Type   TokenType
-	Lexeme string
+	Buffer     *[]scanner.Token
+	Type       TokenType
+	Lexeme     string
+	Start, End int
 }
 
 // Lexer ...
 type Lexer struct {
 	Buffer []scanner.Token
-	Cursor Pos
+	Errors []error
+	Offset int
+	Curr   Token
 }
 
 // New ...
@@ -47,13 +88,201 @@ func New(s *scanner.Scanner) (*Lexer, error) {
 		return nil, ErrNilScanner
 	}
 	buf := []scanner.Token{}
-	for s.Scan() {
-		t := s.Token()
-		buf = append(buf, t)
+	buf, ok := s.ScanAll()
+	if !ok {
+		err := errors.Join(s.Errors...)
+		return nil, err
 	}
 	l := Lexer{
-		Cursor: Zero,
+		Offset: 0,
 		Buffer: buf,
+		Curr: Token{
+			Buffer: nil,
+			Type:   Undef,
+			Lexeme: "",
+			Start:  0,
+			End:    0,
+		},
 	}
 	return &l, nil
+}
+
+// Token ...
+func (l *Lexer) Token() Token {
+	return l.Curr
+}
+
+// Next ...
+func (l *Lexer) Next() bool {
+	if l.Offset > len(l.Buffer)-1 {
+		return false
+	}
+	t := l.Buffer[l.Offset]
+	switch r := t.Rune; {
+	case IsKeyChar(r):
+		return l.nextKeyChar()
+	case IsQuote(r):
+		return l.nextString()
+	case IsDigit(r):
+		return l.nextInteger()
+	case IsWhitespace(r):
+		return l.nextWhitespace()
+	default:
+		l.Errors = append(l.Errors, ErrDisallowedChar)
+		return false
+	}
+}
+
+// IsNewline ...
+func IsNewline(r rune) bool {
+	return r == '\n' || r == '\r'
+}
+
+func (l *Lexer) nextString() bool {
+	t := l.Buffer[l.Offset]
+	tq := t.Rune
+	ss := []string{}
+	ss = append(ss, string(t.Rune))
+	start := l.Offset
+	l.Offset++
+	for {
+		if l.Offset > len(l.Buffer)-1 {
+			lx := strings.Join(ss, "")
+			l.Curr = Token{
+				Buffer: &l.Buffer,
+				Type:   Undef,
+				Lexeme: lx,
+				Start:  start,
+				End:    l.Offset + 1,
+			}
+			l.Errors = append(l.Errors, ErrUnterminatedString)
+			return false
+		}
+		t = l.Buffer[l.Offset]
+		if IsNewline(t.Rune) {
+			lx := strings.Join(ss, "")
+			l.Curr = Token{
+				Buffer: &l.Buffer,
+				Type:   Undef,
+				Lexeme: lx,
+				Start:  start,
+				End:    l.Offset + 1,
+			}
+			l.Errors = append(l.Errors, ErrDisallowedChar)
+			return false
+		}
+		if t.Rune == tq {
+			ss = append(ss, string(t.Rune))
+			l.Offset++
+			break
+		}
+		ss = append(ss, string(t.Rune))
+		l.Offset++
+	}
+	lx := strings.Join(ss, "")
+	l.Curr = Token{
+		Buffer: &l.Buffer,
+		Type:   String,
+		Lexeme: lx,
+		Start:  start,
+		End:    l.Offset,
+	}
+	return true
+}
+
+func (l *Lexer) nextWhitespace() bool {
+	t := l.Buffer[l.Offset]
+	ss := []string{}
+	ss = append(ss, string(t.Rune))
+	start := l.Offset
+	l.Offset++
+	for {
+		if l.Offset > len(l.Buffer)-1 {
+			break
+		}
+		t = l.Buffer[l.Offset]
+		if !IsWhitespace(t.Rune) {
+			break
+		}
+		ss = append(ss, string(t.Rune))
+		l.Offset++
+	}
+	lx := strings.Join(ss, "")
+	l.Curr = Token{
+		Buffer: &l.Buffer,
+		Type:   Whitespace,
+		Lexeme: lx,
+		Start:  start,
+		End:    l.Offset,
+	}
+	return true
+}
+
+// IsWhitespace ...
+func IsWhitespace(r rune) bool {
+	return r == ' ' || r == '\t'
+}
+
+func (l *Lexer) nextInteger() bool {
+	t := l.Buffer[l.Offset]
+	ts := []string{}
+	ts = append(ts, string(t.Rune))
+	start := l.Offset
+	l.Offset++
+	for {
+		if l.Offset > len(l.Buffer)-1 {
+			break
+		}
+		t = l.Buffer[l.Offset]
+		if !IsDigit(t.Rune) {
+			break
+		}
+		ts = append(ts, string(t.Rune))
+		l.Offset++
+	}
+	lx := strings.Join(ts, "")
+	l.Curr = Token{
+		Buffer: &l.Buffer,
+		Type:   Integer,
+		Lexeme: lx,
+		Start:  start,
+		End:    l.Offset,
+	}
+	return true
+}
+
+// IsDigit ...
+func IsDigit(r rune) bool {
+	return unicode.IsDigit(r)
+}
+
+// IsKeyChar ...
+func IsKeyChar(r rune) bool {
+	_, ok := KeyCharMap[r]
+	return ok
+}
+
+// IsQuote ...
+func IsQuote(r rune) bool {
+	return r == '"' || r == '\''
+}
+
+func (l *Lexer) nextKeyChar() bool {
+	t := l.Buffer[l.Offset]
+	tp, ok := KeyCharMap[t.Rune]
+	if !ok {
+		l.Errors = append(l.Errors, ErrKeyCharUnsupported)
+		return false
+	}
+	l.Curr = Token{
+		Buffer: &l.Buffer,
+		Type:   tp,
+		Lexeme: string(t.Rune),
+		Start:  l.Offset,
+		End:    l.Offset + 1,
+	}
+	if ok {
+		l.Offset++
+	}
+	return true
 }
---
+++ lexer/lexer_test.go
@@ -0,0 +1,25 @@
+package lexer
+
+import (
+	"log"
+	"strings"
+	"testing"
+
+	"github.com/mdm-code/scanner"
+)
+
+func TestXxx(t *testing.T) {
+	r := strings.NewReader(".[123 : 1]['\"foo.bar\"'][\"\"][0]")
+	s, err := scanner.New(r)
+	if err != nil {
+		t.Fatal("failed to initialize the scanner")
+	}
+	l, err := New(s)
+	if err != nil {
+		t.Fatal("failed to initialize the lexer")
+	}
+	for l.Next() {
+		log.Println(l.Token())
+	}
+	log.Println(l.Errors)
+}
