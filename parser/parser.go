f4650f8d064aa1050ad0f598a9af734a4bbb9ede
mdm-code <code.madamczyk@gmail.com>
2023-11-12 17:44:11 +0100

    Setup parser for development

--- parser/parser.go
+++ parser/parser.go
@@ -1,14 +1,35 @@
-package parser
-
 /*
+Package parser ...
 
 GRAMMAR
-=======
-
-expression  -> query ;
-query       -> ( transform )* ;
-transform   -> IDENTITY | selector ;
-selector    -> "[" ( STRING | INTEGER | range ) "]" ;
-range       -> INTEGER ":" INTEGER ;
-
+-------
+root        = query
+query       = *(filter)
+filter      = identity / selector
+identity    = DOT
+selector    = '[' *( STRING / INTEGER / range ) ']'
+range       = *INTEGER ':' *INTEGER
 */
+package parser
+
+import "github.com/mdm-code/tq/lexer"
+
+// Parser ...
+type Parser struct {
+	Buffer []lexer.Token
+}
+
+// New ...
+func New(l *lexer.Lexer) (*Parser, error) {
+	buf := []lexer.Token{}
+	for l.Next() {
+		buf = append(buf, l.Token())
+	}
+	p := Parser{
+		Buffer: buf,
+	}
+	return &p, nil
+}
+
+func (p *Parser) parse() {
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,6 +1,28 @@
 package parser
 
-import "testing"
+import (
+	"log"
+	"strings"
+	"testing"
+
+	"github.com/mdm-code/scanner"
+	"github.com/mdm-code/tq/lexer"
+)
 
 func TestXxx(t *testing.T) {
+	q := ".['foo']['bar'][0][1:]"
+	r := strings.NewReader(q)
+	s, err := scanner.New(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	l, err := lexer.New(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	p, err := New(l)
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Println(p.Buffer)
 }
