baee068d495215b1ca3390fbe87ed4b8be8e2eb0
mdm-code <code.madamczyk@gmail.com>
2023-11-21 22:22:02 +0100

    Added unit test for lexer initialization

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -10,6 +10,19 @@ import (
 	"github.com/mdm-code/scanner"
 )
 
+// Verify if the lexer gets initialized with a valid scanner.
+func TestNewLexer(t *testing.T) {
+	r := strings.NewReader("")
+	s, err := scanner.New(r)
+	if err != nil {
+		t.Fatal("failed to initialize the scanner")
+	}
+	_, err = New(s)
+	if err != nil {
+		t.Fatal("failed to initialize the lexer with a valid scanner")
+	}
+}
+
 func TestXxx(t *testing.T) {
 	r := strings.NewReader(".[123 : 1]['\"foo.bar\"][\"\"][0]")
 	s, err := scanner.New(r)
