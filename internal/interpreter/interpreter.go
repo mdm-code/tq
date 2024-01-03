395a06bb796979cd1673200a48609d4d3d553f88
mdm-code <code.madamczyk@gmail.com>
2024-01-04 00:09:27 +0100

    Refactor interpreter based of unit tests

--- internal/interpreter/interpreter.go
+++ internal/interpreter/interpreter.go
@@ -4,13 +4,22 @@ import (
 	"github.com/mdm-code/tq/internal/ast"
 )
 
-// FilterFn specifies the data transformation function type.
-type FilterFn func(data ...interface{}) ([]interface{}, error)
+// FilterFunc specifies the data transformation function type.
+type FilterFunc func(data ...any) ([]any, error)
+
+type filter struct {
+	name  string
+	inner FilterFunc
+}
+
+func (f *filter) call(data ...any) ([]any, error) {
+	return f.inner(data...)
+}
 
 // Interpreter interprets the tq query AST into a pipe-like sequence of
 // filtering functions processing TOML input data as specified in the query.
 type Interpreter struct {
-	filters []FilterFn
+	filters []filter
 }
 
 // New returns a new instance of Interpreter.
@@ -27,13 +36,13 @@ func (i *Interpreter) eval(es ...ast.Expr) {
 // Interpret extracts a sequence of filtering functions by traversing the AST.
 // It returns an entry function that takes in deserialized TOML data and
 // applies filtering functions in the sequence provided by the Interpreter.
-func (i *Interpreter) Interpret(root ast.Expr) FilterFn {
+func (i *Interpreter) Interpret(root ast.Expr) FilterFunc {
 	i.filters = nil // clear out previously accumulated filtering functions
 	i.eval(root)
-	return func(data ...interface{}) ([]interface{}, error) {
+	return func(data ...any) ([]any, error) {
 		var err error
-		for _, fn := range i.filters {
-			data, err = fn(data...)
+		for _, f := range i.filters {
+			data, err = f.call(data...)
 			if err != nil {
 				return data, err
 			}
@@ -62,10 +71,13 @@ func (i *Interpreter) VisitFilter(e ast.Expr) {
 
 // VisitIdentity interprets the Identity AST node.
 func (i *Interpreter) VisitIdentity(e ast.Expr) {
-	identityFn := func(data ...interface{}) ([]interface{}, error) {
-		return data, nil
+	f := filter{
+		name: "identity",
+		inner: func(data ...any) ([]any, error) {
+			return data, nil
+		},
 	}
-	i.filters = append(i.filters, identityFn)
+	i.filters = append(i.filters, f)
 }
 
 // VisitSelector interprets the Selector AST node.
@@ -77,94 +89,106 @@ func (i *Interpreter) VisitSelector(e ast.Expr) {
 // VisitSpan interprets the Span AST node.
 func (i *Interpreter) VisitSpan(e ast.Expr) {
 	s := e.(*ast.Span)
-	spanFn := func(data ...interface{}) ([]interface{}, error) {
-		result := make([]interface{}, 0, len(data))
-		var err error
-		for _, d := range data {
-			switch v := d.(type) {
-			case []interface{}:
-				l, r := s.GetLeft(0), s.GetRight(len(v))
-				if r > len(v) {
-					r = len(v)
+	f := filter{
+		name: "span",
+		inner: func(data ...any) ([]any, error) {
+			result := make([]any, 0, len(data))
+			var err error
+			for _, d := range data {
+				switch v := d.(type) {
+				case []any:
+					l, r := s.GetLeft(0), s.GetRight(len(v))
+					if r > len(v) {
+						r = len(v)
+					}
+					if l > r || l >= len(v) {
+						continue
+					}
+					result = append(result, v[l:r])
+				default:
+					err = ErrTOMLDataType
 				}
-				if l > r || l >= len(v) {
-					continue
-				}
-				result = append(result, v[l:r])
-			default:
-				err = ErrTOMLDataType
 			}
-		}
-		return result, err
+			return result, err
+		},
 	}
-	i.filters = append(i.filters, spanFn)
+	i.filters = append(i.filters, f)
 }
 
 // VisitIterator interprets the Iterator AST node.
 func (i *Interpreter) VisitIterator(e ast.Expr) {
-	iterFn := func(data ...interface{}) ([]interface{}, error) {
-		result := make([]interface{}, 0, len(data))
-		var err error
-		for _, d := range data {
-			switch v := d.(type) {
-			case map[string]interface{}:
-				for _, val := range v {
-					result = append(result, val)
+	f := filter{
+		name: "iterator",
+		inner: func(data ...any) ([]any, error) {
+			result := make([]any, 0, len(data))
+			var err error
+			for _, d := range data {
+				switch v := d.(type) {
+				case map[string]any:
+					for _, val := range v {
+						result = append(result, val)
+					}
+				case []any:
+					for _, val := range v {
+						result = append(result, val)
+					}
+				default:
+					err = ErrTOMLDataType
 				}
-			case []interface{}:
-				for _, val := range v {
-					result = append(result, val)
-				}
-			default:
-				err = ErrTOMLDataType
 			}
-		}
-		return result, err
+			return result, err
+		},
 	}
-	i.filters = append(i.filters, iterFn)
+	i.filters = append(i.filters, f)
 }
 
 // VisitString interprets the String AST node.
 func (i *Interpreter) VisitString(e ast.Expr) {
 	str := e.(*ast.String)
-	strFn := func(data ...interface{}) ([]interface{}, error) {
-		result := make([]interface{}, 0, len(data))
-		var err error
-		for _, d := range data {
-			switch v := d.(type) {
-			case map[string]interface{}:
-				key := str.Trim()
-				res, ok := v[key]
-				if ok {
-					result = append(result, res)
+	f := filter{
+		name: "string",
+		inner: func(data ...any) ([]any, error) {
+			result := make([]any, 0, len(data))
+			var err error
+			for _, d := range data {
+				switch v := d.(type) {
+				case map[string]any:
+					key := str.Trim()
+					res, ok := v[key]
+					if ok {
+						result = append(result, res)
+					}
+				default:
+					err = ErrTOMLDataType
 				}
-			default:
-				err = ErrTOMLDataType
 			}
-		}
-		return result, err
+			return result, err
+		},
 	}
-	i.filters = append(i.filters, strFn)
+	i.filters = append(i.filters, f)
 }
 
 // VisitInteger interprets the Integer AST node.
 func (i *Interpreter) VisitInteger(e ast.Expr) {
 	integer := e.(*ast.Integer)
-	intFn := func(data ...interface{}) ([]interface{}, error) {
-		result := make([]interface{}, 0, len(data))
-		var err error
-		for _, d := range data {
-			switch v := d.(type) {
-			case []interface{}:
-				idx, _ := integer.Vtoi()
-				if idx >= 0 && idx < len(v) {
-					result = append(result, v[idx])
+	f := filter{
+		name: "integer",
+		inner: func(data ...any) ([]any, error) {
+			result := make([]any, 0, len(data))
+			var err error
+			for _, d := range data {
+				switch v := d.(type) {
+				case []any:
+					idx, _ := integer.Vtoi()
+					if idx >= 0 && idx < len(v) {
+						result = append(result, v[idx])
+					}
+				default:
+					err = ErrTOMLDataType
 				}
-			default:
-				err = ErrTOMLDataType
 			}
-		}
-		return result, err
+			return result, err
+		},
 	}
-	i.filters = append(i.filters, intFn)
+	i.filters = append(i.filters, f)
 }
