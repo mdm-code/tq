6cb63b3986e935908276cb5e968d102234a308d3
mdm-code <code.madamczyk@gmail.com>
2024-01-07 22:26:21 +0100

    Added span GetLeft, GetRight tests

--- internal/ast/expr_test.go
+++ internal/ast/expr_test.go
@@ -38,7 +38,89 @@ func TestAccept(t *testing.T) {
 	}
 }
 
-func TestSpanLeftRight(t *testing.T) {}
+// Verify if left int value of the span is retrieved.
+func TestSpanGetLeft(t *testing.T) {
+	cases := []struct {
+		name     string
+		intValue *Integer
+		want     int
+	}{
+		{
+			name:     "0",
+			intValue: &Integer{Value: "0"},
+			want:     0,
+		},
+		{
+			name:     "99",
+			intValue: &Integer{Value: "99"},
+			want:     99,
+		},
+		{
+			// nil Integer pointer results in the default def value returned
+			name:     "nil-integer",
+			intValue: nil,
+			want:     0,
+		},
+		{
+			// non-convertable string results in the default def value returned
+			name:     "non-convertable",
+			intValue: &Integer{Value: "non-convertable"},
+			want:     0,
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			s := Span{Left: c.intValue}
+			def := 0
+			have := s.GetLeft(def)
+			if have != c.want {
+				t.Errorf("have: %d; want: %d", have, c.want)
+			}
+		})
+	}
+}
+
+// Verify if right int value of the span is retrieved.
+func TestSpanGetRight(t *testing.T) {
+	cases := []struct {
+		name     string
+		intValue *Integer
+		want     int
+	}{
+		{
+			name:     "0",
+			intValue: &Integer{Value: "0"},
+			want:     0,
+		},
+		{
+			name:     "99",
+			intValue: &Integer{Value: "99"},
+			want:     99,
+		},
+		{
+			// nil Integer pointer results in the default def value returned
+			name:     "nil-integer",
+			intValue: nil,
+			want:     0,
+		},
+		{
+			// non-convertable string results in the default def value returned
+			name:     "non-convertable",
+			intValue: &Integer{Value: "non-convertable"},
+			want:     0,
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			s := Span{Right: c.intValue}
+			def := 0
+			have := s.GetRight(def)
+			if have != c.want {
+				t.Errorf("have: %d; want: %d", have, c.want)
+			}
+		})
+	}
+}
 
 // Test if the quoted string values get their quotes trimmed.
 func TestStringTrim(t *testing.T) {
