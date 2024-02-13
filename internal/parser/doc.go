9e7268e943c7265d70e3519011732c16cd82b6f6
mdm-code <code.madamczyk@gmail.com>
2024-02-13 22:36:35 +0100

    Removed rough syntax definition in parser

--- internal/parser/doc.go
+++ internal/parser/doc.go
@@ -1,14 +1,5 @@
 /*
 Package parser provides a Parser struct that parses lexer tokens from the tq
 query into an AST.
-
-GRAMMAR
--------
-root        = query
-query       = *(filter)
-filter      = identity / selector
-identity    = DOT
-selector    = '[' *( STRING / INTEGER / span ) ']'
-span        = *INTEGER ':' *INTEGER
 */
 package parser
