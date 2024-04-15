ad5c269bbe2d5a3792a6393bb03a82d5bc8d1380
mdm-code <code.madamczyk@gmail.com>
2024-04-15 18:28:18 +0200

    Implemented Tq.Run method that processes data

--- tq/tq.go
+++ tq/tq.go
@@ -2,7 +2,12 @@ package tq
 
 import (
 	"io"
+	"strings"
 
+	"github.com/mdm-code/scanner"
+	"github.com/mdm-code/tq/internal/interpreter"
+	"github.com/mdm-code/tq/internal/lexer"
+	"github.com/mdm-code/tq/internal/parser"
 	"github.com/mdm-code/tq/internal/toml"
 )
 
@@ -20,5 +25,44 @@ func New(input io.Reader, output io.Writer, adapter toml.Adapter) *Tq {
 	}
 }
 
-func (t *Tq) Run() {
+func (t *Tq) Run(query string) error {
+	reader := strings.NewReader(query)
+	scanner, err := scanner.New(reader)
+	if err != nil {
+		return err
+	}
+	lexer, err := lexer.New(scanner)
+	if err != nil {
+		return err
+	}
+	parser, err := parser.New(lexer)
+	if err != nil {
+		return err
+	}
+	ast, err := parser.Parse()
+	if err != nil {
+		return err
+	}
+	interpreter := interpreter.New()
+	exec := interpreter.Interpret(ast)
+	var data any
+	err = t.adapter.Unmarshal(t.input, &data)
+	if err != nil {
+		return err
+	}
+	filteredData, err := exec(data)
+	if err != nil {
+		return err
+	}
+	for _, d := range filteredData {
+		bytes, err := t.adapter.Marshal(d)
+		if err != nil {
+			return err
+		}
+		if len(bytes) == 0 {
+			continue
+		}
+		_, err = t.output.Write(bytes)
+	}
+	return nil
 }
