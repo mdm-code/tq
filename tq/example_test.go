918944860de07d8516711a23e426a0aa68ffb0b3
mdm-code <code.madamczyk@gmail.com>
2024-04-16 19:28:36 +0200

    Added example test for the tq API struct

---
+++ tq/example_test.go
@@ -0,0 +1,39 @@
+package tq_test
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/mdm-code/tq/toml"
+	"github.com/mdm-code/tq/tq"
+)
+
+// ExampleTq_Run demonstrates how to use the Tq struct to run a query against
+// TOML data. The example uses a TOML configuration file with two servers and
+// queries the IP addresses of the servers. The output is written to the
+// output.
+func ExampleTq_Run() {
+	input := strings.NewReader(`
+[servers]
+
+[servers.alpha]
+ip = "10.0.0.1"
+role = "frontend"
+
+[servers.beta]
+ip = "10.0.0.2"
+role = "backend"
+`)
+	var output bytes.Buffer
+	query := "['servers'][]['ip']"
+	config := toml.GoTOMLConf{}
+	goToml := toml.NewGoTOML(config)
+	adapter := toml.NewAdapter(goToml)
+	tq := tq.New(adapter)
+	_ = tq.Run(input, &output, query)
+	fmt.Println(output.String())
+	// Output:
+	// '10.0.0.1'
+	// '10.0.0.2'
+}
