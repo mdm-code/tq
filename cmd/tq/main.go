cccee962ba6158107ebae83ebfcad0643709bfb9
mdm-code <code.madamczyk@gmail.com>
2024-04-16 19:58:37 +0200

    Embedded the usage string moved to separate file

--- cmd/tq/main.go
+++ cmd/tq/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"flag"
 	"fmt"
 	"io"
@@ -16,44 +17,8 @@ const (
 )
 
 var (
-	usage = `tq - query TOML configuration files
-
-Usage:
-
-	tq [-qtmsi] [file...]
-
-Options:
-
-	-h, --help              show this help message and exit
-	-q, --query             query to run against the input data (default: '.')
-	-t, --tables-inline     emit tables inline (default: false)
-	-m, --arrays-multiline  emit arrays one element per line (default: false)
-	-s, --indent-symbol     provide the indentation string (default: '  ')
-	-i, --indent-tables     indent tables and array tables (default: false)
-
-Example:
-
-	tq -q '["servers"][]["ip"]' <<EOF
-	[servers]
-
-	[servers.prod]
-	ip = "10.0.0.1"
-	role = "backend"
-
-	[servers.staging]
-	ip = "10.0.0.2"
-	role = "backend"
-	EOF
-
-Output:
-
-	'10.0.0.1'
-	'10.0.0.2'
-
-Tq is a tool for querying TOML configuration files with a sequence of intuitive
-filters. It works as a regular Unix filter program reading input data from the
-standard input and producing results to the standard output.
-`
+	//go:embed usage.txt
+	usage string
 
 	query           string
 	tablesInline    bool
