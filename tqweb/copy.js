7111afd60314d8a9416f37feaf63e4f42d2d5e91
mdm-code <code.madamczyk@gmail.com>
2026-08-10 23:08:56 +0200

    Added tq flag options to the frontend

--- tqweb/copy.js
+++ tqweb/copy.js
@@ -3,9 +3,25 @@
 *
 * @param {string} query - The query provided by the user.
 * @param {string} input - The input provided by the user.
+* @param {HTMLFormElement} form - The playground form with option checkboxes.
 */
-const onCopyClick = async (query, input) => {
-  var output = `<<EOF tq -q '${query}'\n${input}\nEOF`;
+const onCopyClick = async (query, input, form) => {
+  const flags = [];
+  if (form.querySelector('[name="tables-inline"]')?.checked) {
+    flags.push('-t');
+  }
+  if (form.querySelector('[name="arrays-multiline"]')?.checked) {
+    flags.push('-m');
+  }
+  if (form.querySelector('[name="indent-tables"]')?.checked) {
+    flags.push('-i');
+  }
+  const indentSymbol = form.querySelector('[name="indent-symbol"]')?.value ?? '';
+  if (indentSymbol !== '' && indentSymbol !== '  ') {
+    flags.push(`-s '${indentSymbol.replace(/'/g, `'\\''`)}'`);
+  }
+  const flagStr = flags.length ? ` ${flags.join(' ')}` : '';
+  var output = `<<EOF tq -q '${query}'${flagStr}\n${input}\nEOF`;
   try {
     navigator.clipboard.writeText(output);
   } catch (err) {
--- tqweb/main.go
+++ tqweb/main.go
@@ -14,32 +14,49 @@ import (
 	wasmhttp "github.com/nlepage/go-wasm-http-server/v2"
 )
 
-/*
-TODO:
-1. Tq config for GoTOML is sent from the frontend selection component.
-- Checkboxes: send true if checked.
-- Buttons: on click, keep highlighted and add to the payload.
-2. The config and TQ has to be instantiated with each call.
-*/
-
 var (
 	//go:embed views/index.html
 	index  string
 	indexT = template.Must(template.New("index").Parse(index))
 )
 
-func main() {
-	conf := toml.GoTOMLConf{}
+func setupTomlAdapter(r *http.Request) *toml.Adapter {
+	tablesInline := r.FormValue("tables-inline") != ""
+	arraysMultiline := r.FormValue("arrays-multiline") != ""
+	indentSymbol := r.FormValue("indent-symbol")
+	if indentSymbol == "" {
+		indentSymbol = "  "
+	}
+	indentTables := r.FormValue("indent-tables") != ""
+	conf := toml.GoTOMLConf{
+		Encoder: struct {
+			TablesInline    bool
+			ArraysMultiline bool
+			IndentSymbol    string
+			IndentTables    bool
+		}{
+			tablesInline,
+			arraysMultiline,
+			indentSymbol,
+			indentTables,
+		},
+	}
 	goToml := toml.NewGoTOML(conf)
 	adapter := toml.NewAdapter(goToml)
-	tq := tq.New(adapter)
+	return adapter
+}
 
+func main() {
 	http.HandleFunc("/process", func(w http.ResponseWriter, r *http.Request) {
 		var output bytes.Buffer
 		var data struct{ Output string }
+
+		adapter := setupTomlAdapter(r)
+		t := tq.New(adapter)
+
 		input := r.FormValue("input")
 		query := r.FormValue("query")
-		err := tq.Run(strings.NewReader(input), &output, query)
+		err := t.Run(strings.NewReader(input), &output, query)
 		if err != nil {
 			data.Output = err.Error()
 		} else {
